9987a28397cadc21ea18f767ed1bad76d713107b
yutada-degu <151909494+yutada-degu@users.noreply.github.com>
2023-12-08 16:44:36 +0900

    Update Chat.tsx
    343541 → 143d66

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -348,7 +348,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
   }, [messagesEndRef]);
 
   return (
-    <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
+    <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#143d66]">
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
